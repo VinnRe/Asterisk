630bd3c731ff77bc910fed7d85db49730909552d
herseyy <herseyodasco1@gmail.com>
2023-11-08 04:01:05 +0800

    p2p vc

--- main_ui/src/Components/meeting_page/MeetingPage.jsx
+++ main_ui/src/Components/meeting_page/MeetingPage.jsx
@@ -1,87 +1,210 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './styles.css';
 
+
 export const MeetingPage = () => {
-  const videoRef = useRef(null);
-  const audioRef = useRef(null);
 
-  useEffect(() => {
-    // Function to handle resizing video elements
-    function resizeVideoElements() {
-      if (videoRef.current && audioRef.current) {
-          const videoContainerWidth = videoRef.current.offsetWidth;
-          const videoContainerHeight = videoRef.current.offsetHeight;
-          const aspectRatio = 4 / 3; // You can adjust this based on your desired aspect ratio
+  const userID = window.crypto.randomUUID();
 
-          // Calculate the width and height for the video elements
-          let videoWidth = videoContainerWidth;
-          let videoHeight = videoContainerWidth / aspectRatio;
+  // create channel link "?room=asdfafafgbn"
+  const ws = new WebSocket(`ws://localhost:8000/ws/${userID}`)
 
-          // Check if the calculated video height exceeds the container height
-          if (videoHeight > videoContainerHeight) {
-              videoHeight = videoContainerHeight;
-              videoWidth = videoContainerHeight * aspectRatio;
-          }
 
-          // Set the width and height for video elements
-          videoRef.current.style.width = `${videoWidth}px`;
-          videoRef.current.style.height = `${videoHeight}px`;
-          audioRef.current.style.width = `${videoWidth}px`;
-          audioRef.current.style.height = `${videoHeight}px`;
-      }
+
+  // free stun server -- from google
+  const servers = {
+    iceServers: [{
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+    }]
   }
 
-  // Call the resize function when the window is resized
-  window.addEventListener('resize', resizeVideoElements);
+  const localVideoRef = useRef(null);
+  const localAudioRef = useRef(null);
 
-  // Call the resize function on initial render
-  resizeVideoElements();
+  const remoteVideoRef = useRef(null);
+  const remoteAudioRef = useRef(null);
 
-    // Function to start the video and audio streams
+  let localStream;
+  let remoteStream;
+  let pc;
+
+
+  useEffect(() => {
+
+    function resizeVideoElements() {
+      if (localVideoRef.current && localAudioRef.current) {
+        const videoContainerWidth = localVideoRef.current.offsetWidth;
+        const videoContainerHeight = localVideoRef.current.offsetHeight;
+        const aspectRatio = 4 / 3; // You can adjust this based on your desired aspect ratio
+
+        // Calculate the width and height for the video elements
+        let videoWidth = videoContainerWidth;
+        let videoHeight = videoContainerWidth / aspectRatio;
+
+        // Check if the calculated video height exceeds the container height
+        if (videoHeight > videoContainerHeight) {
+            videoHeight = videoContainerHeight;
+            videoWidth = videoContainerHeight * aspectRatio;
+        }
+
+        // Set the width and height for video elements
+        localVideoRef.current.style.width = `${videoWidth}px`;
+        localVideoRef.current.style.height = `${videoHeight}px`;
+        localAudioRef.current.style.width = `${videoWidth}px`;
+        localAudioRef.current.style.height = `${videoHeight}px`;
+      }
+    }
+
+    // Call the resize function when the window is resized
+    window.addEventListener('resize', resizeVideoElements);
+
+    // Call the resize function on initial render
+    resizeVideoElements();
+
+
     async function startStream() {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        // Attach video stream to the video element
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+
+        // Attach video localStream to the video element
+        if (localVideoRef.current) {
+          localVideoRef.current.srcObject = localStream;
         }
 
-        // Attach audio stream to the audio element
-        if (audioRef.current) {
-          audioRef.current.srcObject = stream;
+        // Attach audio localStream to the audio element
+        if (localAudioRef.current) {
+          localAudioRef.current.srcObject = localStream;
         }
 
-        // Create an offer and send it to the backend
-        const pc = new RTCPeerConnection();
-        stream.getTracks().forEach(track => pc.addTrack(track, stream));
-        const offer = await pc.createOffer();
-        await pc.setLocalDescription(offer);
+        ws.onopen = function(event) {
+          console.log("Connection is open");
+          console.log(userID, "has arrived!");
 
-        // Send the offer to the backend
-        const response = await fetch('http://localhost:8000/offer', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            sdp: pc.localDescription.sdp,
-            type: pc.localDescription.type,
-          }),
-        });
+          ws.send(JSON.stringify({"userID":userID}));
+        }
 
-        const data = await response.json();
-        // Set the remote description of the peer connection with the received answer
-        await pc.setRemoteDescription(new RTCSessionDescription(data));
+        ws.onclose = function(event) {
+          console.log("Connection is closed");
+        }
+
+        ws.onmessage = function(event) {
+
+          let obj = JSON.parse(event.data);
+
+          if (obj.type) {
+            if (obj.type === "offer") {
+              console.log("received offer!");
+              createAnswer(userID, obj.offer);
+            } else if (obj.type === "answer") {
+              console.log("received answer!");
+              addAnswer(obj.answer);
+            } else if (obj.type === "candidate") {
+              console.log("received candidate!");
+              if (pc) {
+                pc.addIceCandidate(obj.candidate)
+              }
+            }
+          } else {
+            // another user has joined!!!
+            createOffer(userID);
+            console.log(obj.userID, "has arrived!");
+          }
+        }
+
       } catch (error) {
         console.error('Error accessing media devices: ', error);
       }
     }
 
+    async function createPeerConnection(userID) {
+      pc = new RTCPeerConnection(servers);
+
+      remoteStream = new MediaStream();
+      remoteVideoRef.current.srcObject = remoteStream;
+      remoteAudioRef.current.srcObject = remoteStream;
+
+
+      if (!localStream) {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+
+        // Attach video localStream to the video element
+        if (localVideoRef.current) {
+          localVideoRef.current.srcObject = localStream;
+        }
+
+        // Attach audio localStream to the audio element
+        if (localAudioRef.current) {
+          localAudioRef.current.srcObject = localStream;
+        }
+      }
+
+      localStream.getTracks().forEach((track) => {
+        pc.addTrack(track, localStream)
+      })
+
+      pc.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          remoteStream.addTrack(track)
+        })
+      }
+
+      pc.onicecandidate = async (event) => {
+        if (event.candidate) {
+          // console.log(event.candidate);
+          ws.send(JSON.stringify({
+            "type":"candidate", 
+            "candidate":event.candidate
+          }))
+        }
+      }
+    }
+
+
+    async function createOffer(userID) {
+
+      await createPeerConnection(userID);
+
+      const offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+
+
+      // send the offer to peer (offer, userID)
+      ws.send(JSON.stringify({
+        "type":"offer", 
+        "offer":offer,
+        "userID": userID
+      }))
+    }
+
+    async function createAnswer(userID, offer) {
+      await createPeerConnection(userID);
+
+      await pc.setRemoteDescription(offer);
+
+      let answer = await pc.createAnswer();
+      await pc.setLocalDescription(answer);
+
+      // send back the answer to other peer (answer, userID)
+      ws.send(JSON.stringify({
+        "type":"answer",
+        "answer":answer,
+        "userID":userID
+      }))
+    }
+
+    async function addAnswer(answer) {
+      if (!pc.currentRemoteDescription) {
+        pc.setRemoteDescription(answer)
+      }
+    }
+
     startStream();
+
     // Cleanup the event listener when the component is unmounted
     return () => {
       window.removeEventListener('resize', resizeVideoElements);
     };
+
   }, []);
 
   return (
@@ -90,8 +213,11 @@ export const MeetingPage = () => {
         <h1>Asterisk - Video Meeting App</h1>
         <div className="video-container">
           {/* Add video elements here */}
-          <video ref={videoRef} autoPlay playsInline className="video-element"></video>
-          <audio ref={audioRef} autoPlay playsInline className="audio-element"></audio>
+          <video ref={localVideoRef} autoPlay playsInline className="video-element"></video>
+          <audio ref={localAudioRef} autoPlay playsInline className="audio-element"></audio>
+
+          <video ref={remoteVideoRef} autoPlay playsInline className="video-element"></video>
+          <audio ref={remoteAudioRef} autoPlay playsInline className="audio-element"></audio>
         </div>
       </header>
     </div>
