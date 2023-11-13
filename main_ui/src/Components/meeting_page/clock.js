8a9e74c2c12e2667673d121e2cdda3223b030012
VinnRe <22-03804@g.batstate-u.edu.ph>
2023-11-13 15:22:08 +0800

    Add Clock

---
+++ main_ui/src/Components/meeting_page/clock.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const Clock = () => {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <div className='clock'>
+            {currentTime.toLocaleTimeString({ hour: '2-digit', minute: '2-digit' })}
+        </div>
+    );
+};
+
+export default Clock;
