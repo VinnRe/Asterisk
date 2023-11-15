e362bc588b26987c074d792b146c8dc1d3578fc5
VinnRe <22-03804@g.batstate-u.edu.ph>
2023-11-15 17:15:46 +0800

    Rework style

--- asterisk/src/Components/Pages/home_page/home.jsx
+++ asterisk/src/Components/Pages/home_page/home.jsx
@@ -1,104 +1,186 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import accountIcon from '../../Assets/home_assets/account-icon.svg';
-import joinIcon from '../../Assets/home_assets/join-icon.svg';
-import addIcon from '../../Assets/home_assets/add-icon.svg';
 import '../../Styles/home_styles.css';
 
 export const HomePage = () => {
-    useEffect(() => {
-        const anchor = document.querySelector(".button__anchor");
-        const backButton = document.querySelector(".form-button--back");
-        const buttonConferenceJoin = document.querySelector(".button--conference-join");
-        const buttonConferenceCreate = document.querySelector(".button--conference-create");
-        const formHidden = document.querySelector(".form--hidden");
-    
-        if (anchor && backButton && buttonConferenceJoin && buttonConferenceCreate && formHidden) {
-          // Displays Form and Hides Join and Create Button
-          anchor.addEventListener("click", function(event) {
+  useEffect(() => {
+    const sidebar = document.querySelector(".sidebar");
+    const opaqueBackground = document.querySelector(".opaque-background");
+    const settingsButton = document.querySelector(".settings-button");
+    const settingsOverlay = document.querySelector(".settings-overlay");
+    const settingsExitButton = document.querySelector(".settings-exit-button");
+    const joinButton = document.querySelector(".join-button");
+    const formOverlay = document.querySelector(".form-overlay");
+    const backButton = document.querySelector(".back-button");
+    const accountButton = document.querySelector(".account-button");
+    const accountOverlay = document.querySelector(".account-overlay");
+    const accountExitButton = document.querySelector(".account-exit-button");
+
+    function toggleSidebar(mouseEvent, isExpanded) {
+        sidebar.addEventListener(mouseEvent, (event) => {
             event.preventDefault();
-    
-            buttonConferenceJoin.style.display = "none";
-            buttonConferenceCreate.style.display = "none";
-    
-            formHidden.style.display = "block";
-          });
-    
-          // Displays Join and Create Button and Hides Form
-          backButton.addEventListener("click", function(event) {
+            sidebar.style.width = isExpanded ? "10rem" : "2.8rem";
+        });
+    }
+
+    toggleSidebar("mouseover", true);
+    toggleSidebar("mouseout", false);
+
+    function toggleOverlay(addEvent, value, visibility, opacity) {
+        addEvent.addEventListener("click", (event) => {
             event.preventDefault();
-    
-            formHidden.style.display = "none";
-    
-            buttonConferenceJoin.style.display = "block";
-            buttonConferenceCreate.style.display = "block";
-          });
-        }
-      }, []); // Empty dependency array ensures this effect runs once after the initial render
-    
-      return (
-        <body className="home-page">
-          <div className="page-container">
-            <header>
-              <h1 className="header__name--large">Asterisk</h1>
-        
-              <div className="card--icon-account">
-                <a className="button__anchor--icon-account" href="">
-                  <img className="img--icon-account" src={accountIcon} alt="Account Icon" />
-                  <p>Account Name</p>
-                </a>
-              </div>
-            </header>
-        
-            <main>
-              <section className="section section--conference">
-                <div className="button--conference button--conference-join">
-                  <Link to="/join">
-                    <img className="img--conference" src={joinIcon} alt="Join Icon" />
-                    <p>Join</p>
-                  </Link>
-                </div>
-        
-                <form className="form form--hidden" action="">
-                  <label htmlFor="conference-join">Enter Meeting Code</label>
-                  <input
-                    className="form__input"
-                    type="text"
-                    name="conference-join"
-                    id="join-conference"
-                    placeholder="Code"
-                    autoFocus
-                  />
-                  <div className="form__buttons">
-                    <input className="form__submit" type="submit" value="Enter" />
-                    <button className="form-button--back">Back</button>
+            opaqueBackground.style.visibility = visibility;
+            opaqueBackground.style.opacity = opacity;
+            value.style.visibility = visibility;
+            value.style.opacity = opacity;
+        });
+    }
+
+    toggleOverlay(settingsButton, settingsOverlay, "visible", "1");
+    toggleOverlay(settingsExitButton, settingsOverlay, "hidden", "0");
+    toggleOverlay(joinButton, formOverlay, "visible", "1");
+    toggleOverlay(backButton, formOverlay, "hidden", "0");
+    toggleOverlay(accountButton, accountOverlay, "visible", "1");
+    toggleOverlay(accountExitButton, accountOverlay, "hidden", "0");
+}, []);
+
+return (
+      <html lang="en">
+          <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <title>Asterisk</title>
+              <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+              <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet' />
+          </head>
+          <body className="body--h">
+              <header className="header--h">
+                  <h1>Asterisk</h1>
+              </header>
+
+              <main className="main--h">
+                  <section className="main-section">
+                      <div className="controls-container">
+                          <a className="join-button" href="">
+                              <span className="material-icons">groups</span>
+                              <p>Join Conference</p>
+                          </a>
+
+                          <a href="">
+                            <Link to="/create">
+                              <span className="material-icons">add</span>
+                              <p>Create Conference</p>
+                            </Link>
+                          </a>
+                      </div>
+                  </section>
+
+                  <section className="main-section">
+                      <div className="feature-container">
+                          <div className="feature-button">Feature 1</div>
+                          <div className="feature-button">Feature 2</div>
+                          <div className="feature-button">Feature 3</div>
+                      </div>
+                  </section>
+              </main>
+
+              <aside className="sidebar">
+                  <div className="logo">
+                      <span className="material-icons | logo-icon">emergency</span>
+                      <h2>Asterisk</h2>
                   </div>
-                </form>
-        
-                <div className="button--conference button--conference-create">
-                  <Link to="/create">
-                    <img className="img--conference" src={addIcon} alt="Add Icon" />
-                    <p>Create</p>
-                  </Link>
-                </div>
+
+                  <nav className="nav--h">
+                      <ul>
+                          <li>
+                              <a className="dashboard-button" href="">
+                                  <i className="material-icons">space_dashboard</i>
+                                  <span className="icon-text">Dashboard</span>
+                              </a>
+                          </li>
+
+                          <li>
+                              <a className="account-button" href="">
+                                  <i className="material-icons">account_circle</i>
+                                  <span className="icon-text">Account</span>
+                              </a>
+                          </li>
+
+                          <li>
+                              <a className="settings-button" href="">
+                                  <i className="material-icons">settings</i>
+                                  <span className="icon-text">Settings</span>
+                              </a>
+                          </li>
+                      </ul>
+                  </nav>
+              </aside>
+
+              <section className="opaque-background">
+                  <div className="dashboard-overlay">
+                      <h2>Dashboard</h2>
+                  </div>
+
+                  <div className="account-overlay">
+                      <div className="profile">
+                          <div className="profile-icon | material-icons">face</div>
+                          <p className="profile-name">Name</p>
+                      </div>
+
+                      <a href="">
+                          <span className="material-icons">edit</span>
+                          <span>Edit Profile</span>
+                      </a>
+
+                      <a href="">
+                          <span className="material-icons">logout</span>
+                          <span>Logout</span>
+                      </a>
+
+                      <div className="account-exit-button | exit-button">
+                          <span className="material-icons">close</span>
+                      </div>
+                  </div>
+
+                  <div className="settings-overlay">
+                      <h2>Settings</h2>
+
+                      <a href="">
+                          <span className="material-icons">speaker</span>
+                          <span>Audio</span>
+                      </a>
+
+                      <a href="">
+                          <span className="material-icons">videocam</span>
+                          <span>Video</span>
+                      </a>
+
+                      <a href="">
+                          <span className="material-icons">tune</span>
+                          <span>General</span>
+                      </a>
+
+                      <div className="settings-exit-button | exit-button">
+                          <span className="material-icons">close</span>
+                      </div>
+                  </div>
+
+                  <div className="form-overlay">
+                      <h2>Enter Meeting Code</h2>
+                      <div className="form-buttons">
+                          <input className="form__input" type="text" name="join-conference" autoFocus />
+                          <a href="" className="join">
+                              <span>Join</span>
+                          </a>
+                          <a href="" className="back-button">
+                              <span>Back</span>
+                          </a>
+                      </div>
+                  </div>
               </section>
-        
-              <section className="section section--features">
-                <button className="button--feature">
-                  <p>Feature 1</p>
-                </button>
-        
-                <button className="button--feature">
-                  <p>Feature 2</p>
-                </button>
-        
-                <button className="button--feature">
-                  <p>Feature 3</p>
-                </button>
-              </section>
-            </main>
-          </div>
-        </body>
-      );      
+              <script src="script.js"></script>
+          </body>
+      </html>
+);      
 };
 
